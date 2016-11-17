8341c2d25a3915b1762602a597723db01b03397f
wout <wout@impinc.co.uk>
2016-11-17 16:50:25 +0100

    Initial commit

---
+++ src/Bulckens/CliTools/Output.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Bulckens\CliTools;
+
+abstract class Output {
+
+  // Output a message (black)
+  public static function message( $message ) {
+    echo Style::end( $message, true );
+  }
+
+  // Just FYI (light grey)
+  public static function hey( $message ) {
+    self::message( Style::grey( $message ) );
+  }
+
+  // Happy notification (green)
+  public static function good( $message ) {
+    self::message( Style::green( $message ) );
+  }
+
+  // Notify but leave it at that (yellow)
+  public static function meh( $message ) {
+    self::message( Style::yellow( $message ) );
+  }
+
+  // Something is going wrong (red); always exits
+  public static function bad( $message ) {
+    self::message( Style::red( $message ) );
+    exit;
+  }
+  
+}
---
+++ src/Bulckens/CliTools/Style.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Bulckens\CliTools;
+
+abstract class Style {
+
+  // Reset to plain
+  public static function end( $output, $newline = false ) {
+    $output = "$output\033[30m";
+
+    if ( $newline )
+      $output .= "\n";
+
+    return $output;
+  }
+
+  public static function grey( $output ) {
+    return "\033[90m$output";
+  }
+
+  public static function green( $output ) {
+    return "\033[32m$output";
+  }
+
+  public static function yellow( $output ) {
+    return "\033[33m$output";
+  }
+
+  public static function red( $output ) {
+    return "\033[31m$output";
+  }
+
+  public static function purple( $output ) {
+    return "\033[35m$output";
+  }
+
+  public static function colored( $type, $output ) {
+    switch ( $type ) {
+      case 'error':   $color = 'red';    break;
+      case 'warning': $color = 'yellow'; break;
+      case 'good':    $color = 'green';  break;
+      case 'alert':   $color = 'purple'; break;
+      default:        $color = 'grey';   break;
+    }
+
+    return self::$color( $output );
+  }
+  
+}
