7ecd5abcf6cd02d4c57bb61ae784f97bae71cff3
W <wout@users.noreply.github.com>
2018-04-23 09:54:09 +0200

    Silent output on OS test

--- src/Bulckens/CliTools/System.php
+++ src/Bulckens/CliTools/System.php
@@ -6,9 +6,9 @@ abstract class System {
 
   // Detect OS
   public static function os() {
-    if ( exec( 'sw_ver' )) {
+    if ( exec( 'sw_ver > /dev/null  &' )) {
       return 'macOS';
-    } elseif ( exec( 'lscpu' )) {
+    } elseif ( exec( 'lscpu > /dev/null  &' )) {
       return 'linux';
     } else {
       return 'other';
